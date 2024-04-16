8b6c62d32a04a5f1910c98ca0f92c042039cdd8f
MrKoBo <kdtate4@memphis.edu>
2024-04-15 20:51:09 -0500

    Made all pages and connected them all

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  def home
+    render :home
+  end
+  def about
+    render :about
+  end
+  def work
+    render :work
+  end
+  def contact
+    render :contact
+  end
+
+end
